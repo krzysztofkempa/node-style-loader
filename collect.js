f525ac9e673cf18b6372730ee13f7ac2f4fea46b
kkempa <kkempa@olx.pl>
2019-11-28 11:49:18 +0100

    Remove style stack - use global variables instead. Add function to apply only styles matching generated html

--- collect.js
+++ collect.js
@@ -1,30 +1,107 @@
-var styleStack = require('./lib/styleStack');
-// it's necessary setting initialStyleStack as it may not be required as the same module between webpack and the user
-// due to path differences in certain scenarios
-global.initialStyleStack = (global.initialStyleStack !== undefined) ? global.initialStyleStack : new styleStack();
+let allSelectorsWithStyles = [];
+let commonStyles = '';
+let classToSelectorMapping = null;
 
-// initial style collection
-exports.add = add.bind(null, initialStyleStack);
+exports.add = add;
 
-exports.collectInitial = function collectInitial() {
-  var styleTag = initialStyleStack.getStyleTag();
+exports.getMatchingStyles = function (HTML) {
   exports.add = inactiveAdd;
-  // commented-out so it doesn't have to be stored by the user and to test hot-reload
-  //initialStyleStack = undefined;
-  return styleTag;
+  return matchStyles(HTML);
 };
-
-exports.getStyles = function collectInitial() {
-  var styles = initialStyleStack.getStyles();
-  exports.add = inactiveAdd;
-  // commented-out so it doesn't have to be stored by the user and to test hot-reload
-  //initialStyleStack = undefined;
-  return styles;
-};
-
-function add(stack, list, options) {
-  var styles = styleStack.listToStyles(list);
-  stack.addStylesToStack(styles, options);
-}
 
 function inactiveAdd() {}
+
+function add(list) {
+  addStylesToStack(list);
+}
+
+function addStylesToStack(list) {
+  const styles = listToStyles(list);
+  allSelectorsWithStyles = [...allSelectorsWithStyles, ...styles];
+}
+
+function generateMapping() {
+  classToSelectorMapping = allSelectorsWithStyles.reduce((mapping, item, index) => {
+    const classes = item.match(/\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*/g);
+
+    if (classes) {
+      classes.map(c => c.slice(1)).forEach(function (cssClass) {
+        const currentMap = mapping[cssClass];
+        if (currentMap) {
+          currentMap.push(index);
+        } else {
+          mapping[cssClass] = [index];
+        }
+      });
+    } else {
+      commonStyles += item;
+    }
+
+    return mapping
+  }, {});
+}
+
+function matchStyles(HTML) {
+  if (!classToSelectorMapping) {
+    generateMapping();
+  }
+
+  const regex = /class=\"(.*?)\"/g;
+  const existingClasses = HTML.match(regex).reduce((acc, el) => {
+    const classesString = el.split('"')[1];
+    const classes = classesString && classesString.split(' ');
+    if (classes) {
+      return [...acc, ...classes];
+    }
+    return acc;
+  }, []);
+
+  const mapping = existingClasses.reduce((acc, classname) => {
+    const map = classToSelectorMapping[classname];
+    map && map.forEach(key => acc[key] = 1);
+    return acc;
+  }, {});
+
+  const indexes = Object.keys(mapping).sort();
+  const styles = commonStyles + indexes.reduce((css, index) => css + allSelectorsWithStyles[index], '');
+
+  return `<style class="server-style-loader-element">${styles}</style>`;
+}
+
+function listToStyles(list) {
+  let styles = [];
+
+  for (var i = 0; i < list.length; i++) {
+    var item = list[i];
+    var id = item[0];
+    var css = item[1];
+    if (css) {
+      const selectorsWithStyles = extract(css);
+      styles = [...styles, ...selectorsWithStyles];
+    }
+  }
+
+  return styles;
+}
+
+function extract(css) {
+  const result = [];
+  let currentStyle = '';
+  let openedBracketsCount = 0;
+  for (let c of css) {
+    currentStyle += c;
+    switch (c) {
+      case '{':
+        openedBracketsCount++;
+        break;
+      case '}':
+        openedBracketsCount--;
+        if (openedBracketsCount <= 0) {
+          result.push(currentStyle);
+          currentStyle = ''
+        }
+        break;
+    }
+  }
+  return result;
+}
